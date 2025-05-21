282818d99dd2febcd9ff5aef9298e7b630fcc825
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-05-21 17:27:16 +0700

    add log to see token

--- src/app/api/webhookNhanhVN/route.js
+++ src/app/api/webhookNhanhVN/route.js
@@ -8,9 +8,11 @@ export async function POST(request) {
     let token;
     if (process.env.DB_TYPE === 'mysql') {
         token = await getConditionByName("config")
+
     } else if (process.env.DB_TYPE === 'sqlite') {
         token = condition.token;
     }
+    console.log("token: ", token);
 
     try {
         const body = await request.json();
